56c236e79d9134e7a4db4723c9362571af69fe18
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-12-18 02:37:09 +0900

    :wrench: 動的コンポーネント おためし

--- vue-dynamic-component-test/src/App.vue
+++ vue-dynamic-component-test/src/App.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import Input from "@/components/Input.vue";
 import Radio from "@/components/Radio.vue";
 import Select from "@/components/Select.vue";
+
+const currentComponent = ref(Input);
+const currentComponentName = ref("Input");
+let currentComponentIndex = 0;
+const componentList = [
+  { name: "Input", component: Input },
+  { name: "Radio", component: Radio },
+  { name: "Select", component: Select },
+];
+
+const onClick = () => {
+  currentComponentIndex++;
+  if (currentComponentIndex >= componentList.length) {
+    currentComponentIndex = 0;
+  }
+  currentComponent.value = componentList[currentComponentIndex].component;
+  currentComponentName.value = componentList[currentComponentIndex].name;
+};
 </script>
 
 <template>
   <div id="app">
-    <Input />
-    <Radio />
-    <Select />
+    <p>currentComponentName: {{ currentComponentName }}</p>
+    <button @click="onClick">コンポーネント切り替え</button>
+    <keep-alive>
+      <component :is="currentComponent"/>
+    </keep-alive>
   </div>
 </template>
